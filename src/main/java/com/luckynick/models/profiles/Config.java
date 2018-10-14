0476d77a7aa58a4e4d19a7243472eed4bc2a7c18
Mykyta <arm454h@gmail.com>
2018-10-14 15:19:26 +0200

    Enabled running test with default configuration.

---
+++ src/main/java/com/luckynick/models/profiles/Config.java
@@ -0,0 +1,20 @@
+package com.luckynick.models.profiles;
+
+import com.luckynick.models.ManageableField;
+import com.luckynick.models.SerializableModel;
+import com.luckynick.shared.IOClassHandling;
+import com.luckynick.shared.SharedUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@IOClassHandling(dataStorage = SharedUtils.DataStorage.DICTIONARY)
+public class Dictionary extends SerializableModel {
+
+    @ManageableField(required = true)
+    public List<String> messages = new ArrayList<>();
+
+    public Dictionary() {
+        setFilename("dictionary_" + SharedUtils.getDateStringForFileName() + SharedUtils.JSON_EXTENSION);
+    }
+}
